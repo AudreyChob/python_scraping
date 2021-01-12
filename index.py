4f7bc43c85517d4b147fc6053e6ef75352388117
CharlieBolot <bolot.charlie@gmail.com>
2021-01-12 11:36:53 +0100

    test 2 remplissage md

--- index.py
+++ index.py
@@ -1,10 +1,8 @@
-# import libraries
+
 from bs4 import BeautifulSoup
 import urllib.request
 import csv
 
-
-# #specifier l' url
 urlpage = 'http://quotes.toscrape.com/'
 page = urllib.request.urlopen(urlpage)
 soup = BeautifulSoup(page, 'html.parser')
@@ -17,52 +15,28 @@ soup = BeautifulSoup(page, 'html.parser')
 # tag = soup.span['class']
 # print(tag)
 
-
 # tag2 = soup.find("span")
 # print(tag2.contents[0])
 
 allQuote = soup.find_all('div', class_="quote")
 
 tag3 = soup.find_all("span")
-allCitation = soup.find('span', class_="text")
+allCitation = soup.find_all('span', class_="text")
 allAuthors = soup.find('small', class_='author')
 allTags = soup.find_all('a', class_='tag')
-
-Table = []
-
-for quote in allQuote:
-        Table += allCitation.contents
-        Table += allAuthors.contents
-        allTags = quote.find_all('a', class_='tag')
-        for tag in allTags:
-            Table += tag.contents
-
-
-
-# for author in allAuthors:
-#     #print(author.string)
-
-
-# for tag in allTags:
-#     #print(tag.string)
-
-#print(Table)
-
-#!/usr/bin/env python
-# -*- coding: UTF8 -*-
- 
  
 # ecrire dans un fichier
 def ecrireDansFichier(path):
-        truc = 'machin'
         fichier = open('quotes\quote.md',"w")
         fichier.writelines("# Citation | Author | Tags\n"  )
         fichier.writelines("# ------------------------------------------------------------------------- | ----------------------- | -------------------------------------- \n"  )
         for quote in allQuote:
-         fichier.writelines("# "+allCitation.contents[0]+" | "+allAuthors.contents[0]+" | ")
-         for tag in allTags:
-            fichier.writelines(" "+tag.contents[0])
-         fichier.writelines("\n")
+                for Citation in allCitation:
+                        fichier.writelines("# "+Citation.string+" | "+allAuthors.contents[0]+" | ")
+                        allTags = quote.find_all('a', class_='tag')
+                        for tag in allTags:
+                         fichier.writelines(" "+tag.contents[0])
+                        fichier.writelines("\n")
         fichier.writelines("## bye \n"  )
         fichier.close()
  
@@ -78,3 +52,36 @@ def lireFichier(path):
  
 ecrireDansFichier("quote.md")
 lireFichier("quote.md")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Table = []
+
+# for quote in allQuote:
+#         Table += allCitation.contents
+#         Table += allAuthors.contents
+        # allTags = quote.find_all('a', class_='tag')
+        # for tag in allTags:
+        #     Table += tag.contents
+
+
+
+# for author in allAuthors:
+#     #print(author.string)
+
+
+# for tag in allTags:
+#     #print(tag.string)
+
+#print(Table)
